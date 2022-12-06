6906c127e587216cac2672fb35019d66452c42d1
Vivien Phang <101629147+vivienphang@users.noreply.github.com>
2022-12-06 22:25:59 +0800

    updated config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,9 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 // Create a new MongoClient
-const connectDB = async () => {
+const connectDB: Function = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`db connected, ${conn.connection.host}`);
   } catch (error) {
     console.log("error in connecting to db");
